2dc9d1b0c558a73f045626d165165f77ef787b5c
Abdul Mustafa <abdulmustafa@live.com>
2024-07-03 12:27:46 -0400

    Added OPENAPI Key

--- backend/config.py
+++ backend/config.py
@@ -6,4 +6,4 @@ class Config:
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     S3_BUCKET = os.getenv('S3_BUCKET', 'memoirs-test')
     REGION_NAME = os.getenv('REGION_NAME', 'us-east-1')
-    OPENAI_API_KEY = "sk-proj-LkzfRvMZEpJTtPi4hsuTT3BlbkFJRumRnRqBaqU5CniPVOkv"
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
